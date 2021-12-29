444889100a2099c13c9bae5b8e03e948a5dc4354
press0@gmail.com <press0@gmail.com>
2021-12-29 15:07:17 -0600

    setUp test method

--- TestCSV2Parquet.py
+++ TestCSV2Parquet.py
@@ -4,31 +4,33 @@ from CSV2Parquet import CSV2Parquet
 
 class TestCSV2Parquet(unittest.TestCase):
 
+    def setUp(self):
+        # assemble
+        self.CSV_FILE = 'resources/prices.csv'
+        self.PARQUET_FILE = 'resources/prices.parquet'
+        self.CSV_FILE2 = 'resources/prices.out.csv'
+        self.CSV_FILE3 = 'resources/prices3.csv'
+        self.data = CSV2Parquet()
+
     def test_csv_to_parquet_is_reversible(self):
         """
         load csv, save to parquet, load parquet, save to 2nd csv file
         verify equality with pandas
         """
 
-        # assemble
-        CSV_FILE = 'resources/prices.csv'
-        PARQUET_FILE = 'resources/prices.parquet'
-        CSV_FILE2 = 'resources/prices.out.csv'
-        data = CSV2Parquet()
-
         # act
-        data.load_from_file(csv_file=CSV_FILE)
-        csv_table = data.get_csv()
+        self.data.load_from_file(csv_file=self.CSV_FILE)
+        csv_table = self.data.get_csv()
         pandas_table = csv_table.to_pandas()
 
         # load csv file, save as parquet file, load parquet file, save as 2nd csv file
-        data.save_csv_as_parquet_file()
-        data.load_from_file(parquet_file=PARQUET_FILE)
-        data.save_parquet_as_csv_file(csv_file=CSV_FILE2)
+        self.data.save_csv_as_parquet_file()
+        self.data.load_from_file(parquet_file=self.PARQUET_FILE)
+        self.data.save_parquet_as_csv_file(csv_file=self.CSV_FILE2)
 
         # load 2nd csv file
-        data.load_from_file(csv_file=CSV_FILE2)
-        csv_table2 = data.get_csv()
+        self.data.load_from_file(csv_file=self.CSV_FILE2)
+        csv_table2 = self.data.get_csv()
         pandas_table2 = csv_table2.to_pandas()
 
         # assert
@@ -42,19 +44,14 @@ class TestCSV2Parquet(unittest.TestCase):
         verify different dataframes are *not* equal
         """
 
-        # assemble
-        CSV_FILE = 'resources/prices.csv'
-        CSV_FILE3 = 'resources/prices3.csv'
-        data = CSV2Parquet()
-
         # act
-        data.load_from_file(csv_file=CSV_FILE)
-        csv_table = data.get_csv()
+        self.data.load_from_file(csv_file=self.CSV_FILE)
+        csv_table = self.data.get_csv()
         pandas_table = csv_table.to_pandas()
 
         # load second csv file
-        data.load_from_file(csv_file=CSV_FILE3)
-        csv_table2 = data.get_csv()
+        self.data.load_from_file(csv_file=self.CSV_FILE3)
+        csv_table2 = self.data.get_csv()
         pandas_table2 = csv_table2.to_pandas()
 
         # assert
